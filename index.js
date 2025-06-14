76115788296b33efcec14cf483467d58bbc13af8
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-06-15 00:45:09 +0300

    adds index.html

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+
+const cats =['Milo','Otis','Garfield'] ;
+
+ function  destructivelyAppendCat() {
+    cats.push('Ralph');
+ }
+ function destructivelyPrependCat() {
+cats.unshift('Bob');
+ }
+ function destructivelyRemoveFirstCat() {
+    cats.shift('Milo');
+ }
+ function destructivelyRemoveLastCat () {
+    cats.pop('Ralph');
+ }
+ 
+ function appendCat(){
+  return [...cats,'Broom']; 
+ }
+
+ function prependCat(){
+    return ['Arnold',...cats];
+ }
+
+ function removeFirstCat (){
+return cats.slice(1,cats.length +1)
+ }
+
+ function removeLastCat(){
+return cats.slice(0,cats.length -1)
+ }
